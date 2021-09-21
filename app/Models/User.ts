731b4c86109f5033d4e658535d2fe44f23dd3f23
Vinhote <thiago.esw@gmail.com>
2021-09-21 20:15:51 -0300

    feat: create User model

---
+++ app/Models/User.ts
@@ -0,0 +1,38 @@
+import { v4 as uuid } from 'uuid'
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
+
+export default class User extends BaseModel {
+  public static selfAssignPrimaryKey = true
+
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public email: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static assignUuid(user: User) {
+    user.id = uuid()
+  }
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
+}
