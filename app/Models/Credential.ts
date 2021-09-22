bfd5e2db58b991100515cc3852483e99c6172c51
Vinhote <thiago.esw@gmail.com>
2021-09-21 22:05:50 -0300

    feat: create model and migration to Credential entity

---
+++ app/Models/Credential.ts
@@ -0,0 +1,39 @@
+import { v4 as uuid } from 'uuid'
+import { DateTime } from 'luxon'
+import { BaseModel, beforeCreate, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import Encryption from '@ioc:Adonis/Core/Encryption'
+
+export default class Credential extends BaseModel {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public username: string
+
+  @column({ serializeAs: null })
+  public password: string
+
+  @column()
+  public description: string
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @beforeCreate()
+  public static assignUuid(credential: Credential) {
+    credential.id = uuid()
+  }
+
+  @beforeSave()
+  public static encryptPassword(credential: Credential) {
+    if (credential.$dirty.password) {
+      credential.password = Encryption.encrypt(credential.password)
+    }
+  }
+}
---
+++ database/migrations/1632271145628_credentials.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Credentials extends BaseSchema {
+  protected tableName = 'credentials'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary()
+      table.string('name').notNullable().index()
+      table.string('username', 255).notNullable().index()
+      table.string('password').notNullable()
+      table.text('description').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
