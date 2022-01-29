c5f3e10fab9604fb53f5595d6a39a68cffa9940a
Vinhote <thiago.esw@gmail.com>
2022-01-29 14:10:50 -0400

    feat: handle exception when attemp throw

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -2,17 +2,21 @@ import { RouteHandler } from '@ioc:Adonis/Core/Route'
 import Env from '@ioc:Adonis/Core/Env'
 
 export default class AuthController {
-  public login: RouteHandler = async ({ auth, request }) => {
+  public login: RouteHandler = async ({ auth, request, response }) => {
     const email = request.input('email')
     const password = request.input('password')
 
-    const token = await auth
+    try {
+      const token = await auth
       .use('api')
       .attempt(email, password, { expiresIn: Env.get('JWT_EXPIRES_IN') })
 
     return {
       user: token.user,
       token: token.token,
+    }
+    } catch (error) {
+      response.unauthorized({ error: { code: error.code, message: error.message } })
     }
   }
 
